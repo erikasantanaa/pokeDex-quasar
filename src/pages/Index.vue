aec1d0587a33e2667c1fdbbe84bac5973cec77f6
Erika Santana <erika.araujo12344@gmail.com>
2022-02-16 20:10:50 -0300

    Inseri funções

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,25 +3,49 @@
   <div class=" column items-center">
     <h2> {{name}} </h2>
     <q-img :src="url" width="250px" />
+
+  <div class="row">
+    <div class="col-4 q-mr-md">
+    <span>Type</span>
+      <div v-for="(type, index) in pokeTypes" :key="index" class="q-pt-sm">
+        <q-badge rounded  color="primary" :label="type" />
+      </div>
+    </div>
+    <div class="col-4 q-ml-md">
+    <span>Ability</span>
+      <div v-for="(ability, idx) in pokeAbilities" :key="idx" class="q-pt-sm">
+        <q-badge rounded  color="secondary" :label="ability" />
+      </div>
+    </div>
+  </div>
   </div>
   <div class="row justify-around full-width">
     <q-input filled v-model="search" label="Digite nome Pokemon" />
     <q-btn color="purple" label="Pesquisar" @click="getPokemon"/>
   </div>
-  <div class="row justify-between full-width">
+  <div class="row justify-between absolute full-width container-arrows">
     <q-icon
       name="far fa-arrow-alt-circle-left"
       color="primary"
+      size="50px"
       class="q-ml-sm cursor-pointer"
-      style="font-size: 4rem"
-    />
+      @click="getPokemon(id - 1)"
+      >
+    <q-tooltip>
+        Anterior
+      </q-tooltip>
+    </q-icon>
     <q-icon
       name="far fa-arrow-alt-circle-right"
       color="primary"
+      size="50px"
       class="q-mr-sm cursor-pointer"
-      style="font-size: 4rem"
-    />
-
+      @click="getPokemon(id + 1)"
+      >
+    <q-tooltip>
+        Próxima
+      </q-tooltip>
+    </q-icon>
   </div>
   </q-page>
 </template>
@@ -35,7 +59,10 @@ export default {
     return {
       name: '',
       url: '',
-      search: 'ditto',
+      id: null,
+      search: 'nidoqueen',
+      pokeTypes: [],
+      pokeAbilities: []
     }
   },
 
@@ -44,15 +71,28 @@ export default {
   },
 
   methods: {
-      getPokemon() {
-            api
-        .get(`/pokemon/${this.search}/`)
+      getPokemon(id) {
+        api
+        .get(id > 0 ? `/pokemon/${id}/` : `/pokemon/${this.search}/`)
         .then((response) => {
+          this.pokeTypes = []
+          this.pokeAbilities = []
+
           //lidar com o sucesso
-          console.log(response);
+          this.id = response.data.id;
           this.name = response.data.name;
           this.url = response.data.sprites.other.dream_world.front_default;
-          this.triggerPositive()
+          this.search = response.data.name;
+
+          const nameTypes = response.data.types
+          nameTypes.forEach(pokemonType => {
+            this.pokeTypes.push(pokemonType.type.name)
+          })
+
+          const nameAbilities = response.data.abilities
+          nameAbilities.forEach(pokemonAbility => {
+            this.pokeAbilities.push(pokemonAbility.ability.name)
+          })
         })
         .catch((error) => {
           //lidar com o erro
@@ -73,9 +113,10 @@ export default {
         this.$q.notify({
           type: 'negative',
           position: "top",
-          message: 'Pokemon não encontrado! Tente novamente!'
+          message: 'Pokemon não encontrado! Você escreveu errado!'
         })
       },
     },
   }
 </script>
+
